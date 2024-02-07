e3a2d22c34d62365b93d94f4af99921416c98e26
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-02-07 16:42:12 +0530

    navbar

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,10 +1,22 @@
 "use client";
 import { UserButton } from "@clerk/nextjs";
+import Container from "../Container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const NavBar = () => {
+  const router = useRouter();
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div
+          className="flex items-center gap-1 cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          <Image src="/logo.png" alt="logo" width="30" height="30" />
+        </div>
+        <UserButton afterSignOutUrl="/" />
+      </Container>
     </div>
   );
 };
