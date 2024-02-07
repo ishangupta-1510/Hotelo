f41e8d0b949ce042cf0b3eab3a71b66624e45495
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-02-07 16:09:25 +0530

    first commit

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+
+const NavBar = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default NavBar;
