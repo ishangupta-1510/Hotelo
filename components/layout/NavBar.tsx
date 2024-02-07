f29ecdba4573a244b7d9b825298f551e0617b777
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-02-07 17:04:10 +0530

    navbar

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,21 +1,43 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import Container from "../Container";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const NavBar = () => {
   const router = useRouter();
+  const { userId } = useAuth();
   return (
     <div className="sticky top-0 border border-b-primary/10 bg-secondary">
       <Container>
-        <div
-          className="flex items-center gap-1 cursor-pointer"
-          onClick={() => router.push("/")}
-        >
-          <Image src="/logo.png" alt="logo" width="30" height="30" />
+        <div className="flex justify-between items-center">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <Image src="/logo.png" alt="logo" width="30" height="200" />
+            <div>Hotelo</div>
+          </div>
+          <div className="flex gap-3 items-center">
+            <div>theme</div>
+            <UserButton afterSignOutUrl="/" />
+            {!userId && (
+              <>
+                <Button
+                  onClick={() => router.push("/sign-in")}
+                  variant="outline"
+                  size="sm"
+                >
+                  Sign In
+                </Button>
+                <Button onClick={() => router.push("/sign-up")} size="sm">
+                  Sign Up
+                </Button>
+              </>
+            )}
+          </div>
         </div>
-        <UserButton afterSignOutUrl="/" />
       </Container>
     </div>
   );
